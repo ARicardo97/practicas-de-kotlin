608982b3346eb190d1f632b20ca5f23f627c5949
Richy <ricardo.matu@axonsolutions.com.mx>
2019-09-27 14:20:53 -0500

    vistas y toolbar resueltas

---
+++ app/src/main/java/com/example/juego_memoria_app/Cartas_imagen.kt
@@ -0,0 +1,12 @@
+package com.example.juego_memoria_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cartas_imagen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cartas_imagen)
+    }
+}
